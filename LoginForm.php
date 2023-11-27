5445ae6a3c3ff6979311bda6c2d85f4f917e3094
Altera <widiansah2401@gmail.com>
2023-11-27 14:42:36 +0700

    Menambah File Login dan Register form

---
+++ LoginForm.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Login</title>
+        <link rel="stylesheet" href="src\style\LoginStyle.css" />
+    </head>
+    <body>
+        <div class="form-structor">
+            <div class="signup">
+                <h2 class="form-title" id="signup">Sign up</h2>
+                <div class="form-holder">
+                    <input type="email" class="input" placeholder="Email" />
+                    <input type="password" class="input" placeholder="Password" />
+                </div>
+                <button class="submit-btn">Login</button>
+                <a href="RegisterForm.php"> Create Account</a>
+            </div>
+        </div>
+    </body>
+</html>
