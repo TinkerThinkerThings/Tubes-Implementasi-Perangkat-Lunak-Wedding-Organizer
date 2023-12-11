4dee5e4a139f9283357db11e7c023eec1d1ed555
Altera <widiansah2401@gmail.com>
2023-12-11 22:35:09 +0700

    Mengubah tampilan login dan register

--- login_form.php
+++ login_form.php
@@ -9,10 +9,9 @@
 <body>
     <div class="form-structor">
         <div class="signup">
-            <h2 class="form-title" id="signup">Sign up</h2>
+            <h2 class="form-title" id="signup">Login</h2>
             <div class="form-holder">
-                <input type="email" class="input" placeholder="Email" />
-                <input type="password" class="input" placeholder="Password" />
+                <input type="email" class="input" placeholder="Email"/>
             </div>
             <button class="submit-btn">Login</button>
             <a href="register_form.php"> Create Account</a>
--- register_form.php
+++ register_form.php
@@ -8,18 +8,23 @@
 
 <body>
     <div class="form-structor">
-        <div class="signup">
-            <h2 class="form-title" id="signup">Sign up</h2>
-            <div class="form-holder">
-                <input type="text" class="input" placeholder="Name" />
-                <input type="text" class="input" placeholder="Alamat" />
-                <input type="email" class="input" placeholder="Email" />
-                <input type="text" class="input" placeholder="Nomor HP" />
-                <input type="password" class="input" placeholder="Password" />
+        <form>
+            <div class="signup">
+                <h2 class="form-title" id="signup">Sign up</h2>
+                <div class="form-holder">
+                    <input type="text" class="input" placeholder="Name" />
+                    <input type="text" class="input" placeholder="Alamat" />
+                        <input type="radio" id="lakiLaki" name="jenisKelamin" value="Laki - laki">
+                        <label for="lakiLaki">Laki - Laki</label></br>
+                        <input type="radio" id="perempuan" name="jenisKelamin" value="Perempuan">
+                        <label for="perempuan">Perempuan</label>
+                    <input type="email" class="input" placeholder="Email" />
+                    <input type="text" class="input" placeholder="Nomor HP" />
+                </div>
+                <button class="submit-btn">Sign up</button>
+                <a href="login_form.php"> Already have account?</a>
             </div>
-            <button class="submit-btn">Sign up</button>
-            <a href="login_form.php"> Already have account?</a>
-        </div>
+        </form>
     </div>
 </body>
 
