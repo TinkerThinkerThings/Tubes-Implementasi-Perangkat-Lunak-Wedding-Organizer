4dcf5808596286e63a38908cea8fae3e789fc5ae
Meepoo <desdjaja@yahoo.com>
2023-12-11 20:46:46 +0700

    menambahkan file admin.php

---
+++ admin.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+include 'koneksi.php';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Dashboard Admin - GreenSide</title>
+  <link rel="stylesheet" href="src/style/global.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,600;1,300;1,600&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css">
+  <link rel="stylesheet" href="	https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+
+  <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/footers/">
+
+  <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+
+<body>
+  <h1>Hello World</h1>
+</body>
+
+</html>
