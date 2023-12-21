36fc11ca793405b1f26cedf20096e4f6c9a93ad3
Meepoo <desdjaja@yahoo.com>
2023-12-21 18:32:59 +0700

    menambahkan file admin.php dan admin.css

---
+++ admin.php
@@ -0,0 +1,68 @@
+<span style="font-family: verdana, geneva, sans-serif;">
+  <!DOCTYPE html>
+  <html lang="en">
+
+  <head>
+    <meta charset="UTF-8" />
+    <title>Admin Dashboard | GreenSide</title>
+    <link rel="stylesheet" href="src/style/admin.css" />
+    <!-- Font Awesome Cdn Link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+
+  </head>
+
+  <body>
+    <div class="container">
+      <nav>
+        <ul>
+          <li><a href="#" class="logo">
+              <img src="src/assets/images/logo.png" alt="GreenSide">
+              <span class="nav-item">GreenSide</span>
+            </a></li>
+          <li><a href="">
+              <i class="fas fa-home" id="beranda"></i>
+              <span class="nav-item">Beranda</span>
+            </a></li>
+          <li><a href="">
+              <i class="fas fa-book"></i>
+              <span class="nav-item">Pesanan</span>
+            </a></li>
+          <li><a href="">
+              <i class="fas fa-wallet"></i>
+              <span class="nav-item">Pembayaran</span>
+            </a></li>
+          <li><a href="">
+              <i class="fas fa-cog"></i>
+              <span class="nav-item">Pengaturan</span>
+            </a></li>
+          <li><a href="">
+              <i class="fas fa-question-circle"></i>
+              <span class="nav-item">Bantuan</span>
+            </a></li>
+          <li><a href="" class="logout">
+              <i class="fas fa-sign-out-alt"></i>
+              <span class="nav-item">Keluar</span>
+            </a></li>
+        </ul>
+      </nav>
+
+      <section class="main">
+        <div class="main-top">
+          <h1>Dashboard</h1>
+          <i class="fas fa-user-cog"></i>
+        </div>
+        <div class="main-dashboard">
+
+        </div>
+
+        <section class="main-order">
+          <h1>Pesanan Terbaru</h1>
+          <div class="list-order-box">
+          </div>
+        </section>
+      </section>
+    </div>
+  </body>
+
+  </html>
+</span>
